0242ad7003dface6376ca02f1074d201072a49f0
XuWeidongCQ <1455049258@qq.com>
2019-09-07 15:57:40 +0800

    vue-cli3.0-basicConfig

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/ bootstrap.bundle.min.js '
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
